293b5e279c06d3c47d436dfe7be74f6e12f322fa
Bhavik <raibhavik7619325139@gmail.com>
2025-06-21 15:56:34 +0000

    fix

---
+++ bhavik-portfolio/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+    <div className="body">
+      <Navbar></Navbar>
+
+    </div>
+    
+      
+    </>
+  )
+}
+
+export default App
---
+++ bhavik-portfolio/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import whatsappIcon from '../assets/whatappsvg.svg';
+import githubicon from '../assets/githubicon.svg';
+import linkedin from '../assets/linkedin.svg';
+
+
+let Navbar = () => {
+    return (
+        <div className="maincontainer">
+        <div className="navcontainer">
+            <div className="icon">
+                <ul>
+                    <li><a href="https://wa.me/qr/SPLZRPUSUPAWG1"><img src={whatsappIcon} alt="whatsappicon" />
+                    </a></li>
+                    <li><a href="https://wa.me/qr/SPLZRPUSUPAWG1"><img src={githubicon} alt="githubicon" />
+                    </a></li>
+                    <li><a href="https://www.linkedin.com/in/bhavik-rai-438a70294?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img src={linkedin} alt="linkedinicon" />
+                    </a></li>
+                </ul>
+
+            </div>
+            <div className="icon">
+
+            </div>
+            <div className="icon">
+
+            </div>
+            <div className="icon">
+
+            </div>
+        </div>
+        </div>
+
+    )
+
+}
+
+export default Navbar;
